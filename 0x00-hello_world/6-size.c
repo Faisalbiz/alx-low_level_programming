7bd506aeb19916b689ae9e0a7a76aa05aa933ce1
Faisal Bizimungu <faisalbizimungu@gmail.com>
2022-04-11 02:18:20 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,10 @@
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
-
-printf("Size of a char: Xlu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of an int: Xlu bytes(s)\n", (unsigned long)sizeof(a));
-printf("Size of a long int: Xlu bytes(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long long int: Xlu bytes(s)\n", (unsigned long)sizeof(c));
-printf("Size of a float: Xlu bytes(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: xd byte(s)\n", sizeof(char));
+printf("Size of an int: xd bytes(s)\n", sizeof(int));
+printf("Size of a long int: xd bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: xd bytes(s)\n", sizeof(long long int));
+printf("Size of a float: xd bytes(s)\n", sizeof(float));
 return (0);
 }
